d80020e3ce9ce1091c14d5dda0d89e0f7b2a1327
Shantanu Ghorai <shantanughorai@ShantanuG-MBP.lan>
2021-01-25 18:14:06 -0600

    Public api and expirty time update

--- app.js
+++ app.js
@@ -10,22 +10,48 @@ app.use(bodyparser.urlencoded({
 }));
 
 app.post('/api/sts', function(req, res) {
-        console.log(req.headers);
-        var clientId = req.body.appId;//req.body.clientId;
-        var clientSecret = req.headers.apikey;//req.body.clientSecret;
-        var options = {
-            "iat": new Date().getTime(),
-            "exp": new Date(new Date().getTime() + 4 * 60 * 60 * 1000).getTime(),
-            "iss": clientId,
-            "sub" : "Intercorp"
-        }
-        var token = jwt.sign(options, clientSecret);
-        res.header("Access-Control-Allow-Origin","*");
-	    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	    res.header("Referrer-Policy","origin-when-cross-origin, strict-origin-when-cross-origin");
-	    res.header("Content-Security-Policy","default-src 'none'");
-        res.send({"jwt":token});
-    });
+  console.log(req.headers);
+  if(req.body && req.body.appId && req.headers && req.headers.apikey){
+    var clientId = req.body.appId;//req.body.clientId;
+    var clientSecret = req.headers.apikey;//req.body.clientSecret;
+    var options = {
+      "iat": new Date().getTime(),
+      "exp": new Date(new Date().getTime() + 20 * 60 * 1000).getTime(),
+      "iss": clientId,
+      "sub" : "Intercorp"
+    }
+    var token = jwt.sign(options, clientSecret);
+    res.header("Access-Control-Allow-Origin","*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  res.header("Referrer-Policy","origin-when-cross-origin, strict-origin-when-cross-origin");
+	  res.header("Content-Security-Policy","default-src 'none'");
+    res.status(200).send({"jwt":token});
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+        
+  });
+
+app.post('/api/public', function(req, res) {
+  console.log(req.headers);
+  if(req.body && req.body.appId && req.headers && req.headers.apikey){
+    var clientId = req.body.appId;//req.body.clientId;
+    var clientSecret = req.headers.apikey;//req.body.clientSecret;
+    var options = {
+      "iat": new Date().getTime(),
+      "exp": new Date(new Date().getTime() + 20 * 60 * 1000).getTime(),
+      "appId": clientId
+    }
+    var token = jwt.sign(options, clientSecret);
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Referrer-Policy","origin-when-cross-origin, strict-origin-when-cross-origin");
+    res.header("Content-Security-Policy","default-src 'none'");
+    res.status(200).send({"jwt":token});
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
 
 console.log("JWT service request received" + port);
 app.listen(port);
